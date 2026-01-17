188b7617778705189f96c568a7feacb45d77faeb
Manikanta Cheruku <cherukumanikanta77@gmail.com>
2026-01-17 12:22:03 +0530

    updated client.ts and mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  typescript: {
+    // This ignores the "f" type errors and finishes the build
+    ignoreBuildErrors: true,
+  },
+  // We removed the 'eslint' and 'experimental' keys to stop the warnings
 };
+
 export default nextConfig;
